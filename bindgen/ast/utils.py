a5d78f65ef0d43fefc30a2df556af1ac6dde2c05
KokaKiwi <kokakiwi@kokakiwi.net>
2015-01-20 08:36:54 +0100

    Add some mod paths util functions.

--- bindgen/ast/utils.py
+++ bindgen/ast/utils.py
@@ -1,4 +1,5 @@
 
+# Module path utils
 def get_parent_modpath(item):
     from bindgen.ast import objects as obj
 
@@ -6,15 +7,30 @@ def get_parent_modpath(item):
         return get_modpath(item.bases[0])
     return get_modpath(item.parent)
 
+def resolve_modpath(modpath, item):
+    if hasattr(modpath, '__call__'):
+        modpath = modpath(item)
+    return modpath
+
 def get_modpath(item):
     if hasattr(item, 'modpath'):
-        modpath = item.modpath
-        if hasattr(modpath, '__call__'):
-            modpath = modpath(item)
-        return modpath
+        return resolve_modpath(item.modpath, item)
     return get_parent_modpath(item)
 
 def submodpath(path):
-    def modpath(item):
+    def _modpath(item):
         return get_parent_modpath(item) + path
-    return modpath
+    return _modpath
+
+def concatmodpaths(*paths):
+    def _modpath(item):
+        modpath = []
+        for path in iter(paths):
+            modpath += resolve_modpath(path, item)
+        return modpath
+    return _modpath
+
+def copymodpath(item):
+    def _modpath(it):
+        return get_modpath(item)
+    return _modpath
