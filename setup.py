c1872c6b3c2dfb543b98b3f4cd80e4825a67775f
KokaKiwi <kokakiwi@kokakiwi.net>
2015-07-31 14:59:38 +0200

    Add entry points (rust-cpp-bindgen executable)

--- setup.py
+++ setup.py
@@ -6,4 +6,9 @@ setup(
     version='0.1.0',
 
     packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'rust-cpp-bindgen = rust_bindgen.main:entry',
+        ],
+    },
 )
